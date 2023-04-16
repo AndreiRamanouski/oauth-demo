52f84bde25179dbf69b8ade4c81bd309e8750600
Andrei Romanouski <@aromanouski1>
2023-04-16 13:25:26 +0300

    Configure eureka

---
+++ discovery-service/src/test/java/com/appdev/discovery/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
